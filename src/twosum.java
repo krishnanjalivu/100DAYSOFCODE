6f79d0bf6e05c05a289bd329d2efe78666a6427b
krishnanjalivu <vukrishnanjali@gmail.com>
2023-05-11 00:37:31 +0530

    twosum

--- src/twosum.java
+++ src/twosum.java
@@ -1,25 +1,58 @@
+import java.util.Arrays;
+
 public class twosum {
-    public int[] twoSum(int[] numbers, int target) {
-        int temp[]=new int[2];
-
-        for(int i=0;i<numbers.length;i++)
+//    public int[] twoSum(int[] numbers, int target) {
+//        int temp[]=new int[2];
+//
+//        for(int i=0;i<numbers.length;i++)
+//        {
+//            for(int j=i+1;j<numbers.length;j++)
+//            {
+//                if(numbers[j]==target-numbers[i])
+//                {
+//                    if(numbers[i]<numbers[j])
+//                    {
+//                        temp[0]=i+1;
+//                        temp[1]=j+1;
+//                        return temp;
+//                    }
+//
+//
+//                }
+//            }
+//        }
+//        return null;
+public static int[] twoSum(int[] numbers, int target) {
+    int temp[]=new int[2];
+    int start=0;
+    int end=numbers.length-1;
+    while(start<end)
+    {
+        int sum=numbers[start]+numbers[end];
+        if(sum==target)
         {
-            for(int j=i+1;j<numbers.length;j++)
-            {
-                if(numbers[j]==target-numbers[i])
-                {
-                    if(numbers[i]<numbers[j])
-                    {
-                        temp[0]=i+1;
-                        temp[1]=j+1;
-                        return temp;
-                    }
-
-
-                }
-            }
+            temp[0]=start+1;
+            temp[1]=end+1;
+            break;
         }
-        return null;
+        else if(sum>target)
+        {
+            end--;
+        }
+        else
+        {
+            start++;
+        }
 
     }
+    return temp;
+}
+
+
+
+    public static void main(String[] args) {
+        int a[]={2,3,5,8,9};
+        System.out.println(twoSum(a,10));
+    }
+
 }
