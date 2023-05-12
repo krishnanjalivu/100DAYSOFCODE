5935d99c4de4897dc11a6258f829c9f0e1ed09a0
krishnanjalivu <vukrishnanjali@gmail.com>
2023-05-12 22:48:44 +0530

    linkedlist

--- src/linkedlistmiddle.java
+++ src/linkedlistmiddle.java
@@ -1,2 +1,13 @@
+import java.util.*;
 public class linkedlistmiddle {
+    public static ListNode middleNode(ListNode head) {
+        ListNode slow =head,fast=head;
+        while(fast!=null && fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+
 }
