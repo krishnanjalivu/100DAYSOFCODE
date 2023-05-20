f9c80e4c269376c570948a83faf5e70ad434b7fb
krishnanjalivu <vukrishnanjali@gmail.com>
2023-05-20 17:57:26 +0530

    1557

---
+++ src/Solution123.java
@@ -0,0 +1,14 @@
+public class Solution123 {
+    public class Solution123 {
+        // you need to treat n as an unsigned value
+        public int hammingWeight(int n) {
+            int ans = 0;
+
+            for (int i = 0; i < 32; ++i)
+                if (((n >> i) & 1) == 1)
+                    ++ans;
+
+            return ans;
+        }
+    }
+}
